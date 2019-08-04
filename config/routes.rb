e337ea29b0c96a377b87710869a9a5099db52b6a
Nataliya R <n.rudenko@gmail.com>
2019-08-04 21:53:27 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ resources :posts, only: [:index, :show, :new, :create, :edit, :update]
  
 
   root 'posts#index'
-get 'authors/:id/posts', to: 'authors#posts_index'
-  get 'authors/:id/posts/:post_id', to: 'authors#post' 
+#get 'authors/:id/posts', to: 'authors#posts_index'
+ # get 'authors/:id/posts/:post_id', to: 'authors#post' 
   
 end
