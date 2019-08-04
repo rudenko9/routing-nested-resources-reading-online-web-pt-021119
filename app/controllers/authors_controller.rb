baf4cd4b471dd81197e39cfd261f53930a418a4a
Nataliya R <n.rudenko@gmail.com>
2019-08-04 21:45:42 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,5 +3,13 @@ class AuthorsController < ApplicationController
   def show
     @author = Author.find(params[:id])
   end
+  def posts_index
+    @author = Author.find(params[:id])
+    @posts = @author.posts
+    render template: 'posts/index'
+  end
+ 
+  def post
+    @author = Author.find(params[:id])
 
 end
